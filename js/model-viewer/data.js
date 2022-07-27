9ef831ca66400e8f62a8af2fe89053132b6155f9
Jiří <satorajirka@seznam.cz>
2022-07-27 19:54:46 +0200

    add 3D models and textures

---
+++ js/model-viewer/data.js
@@ -0,0 +1,16 @@
+const modelsData = [
+    {
+        name: "Chair 1",
+        modelName: "Chair1",
+        srcFolder: "./static/models/chair1",
+        textureVariations: [
+            {
+                name: "clean",
+                folderName: "clean",
+                types: ["albedo", "metallic-smoothness", "metalness", "roughness", "normal"]
+            }
+        ]
+    }
+]
+
+export default modelsData;
--- webpack.common.js
+++ webpack.common.js
@@ -16,14 +16,20 @@ module.exports = {
             template: path.resolve(__dirname, "index.html"),
             inject: true
         }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "3d-modely", "index.html"),
+            filename: "3d-modely/index.html",
+            inject: true
+        }),
         new CopyPlugin({
             patterns: [
                 {
                     from: path.resolve(__dirname, "static").replace(/\\/g, "/"),
-                    to: path.resolve(__dirname, "dist"),
+                    to: path.resolve(__dirname, "dist", "static"),
                     noErrorOnMissing: true
                 }
             ]
         })
     ]
 }
+// todo - později přidat handlebars
