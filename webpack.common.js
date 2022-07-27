ecdc56124461cec732529864c5a965b667e42b2f
Jiří <satorajirka@seznam.cz>
2022-07-27 13:30:27 +0200

    prepare for development

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: './main.js'
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+        filename: "[name].[contenthash].js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "index.html"),
+            inject: true
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "static").replace(/\\/g, "/"),
+                    to: path.resolve(__dirname, "dist"),
+                    noErrorOnMissing: true
+                }
+            ]
+        })
+    ]
+}
---
+++ webpack.prod.js
@@ -0,0 +1,24 @@
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common");
+
+module.exports = merge(commonConfig, {
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin()
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
+});
