aeaa8440159603d96675431fd9e9aecd7a4c2ff6
Jiří <satorajirka@seznam.cz>
2022-08-07 16:36:16 +0200

    create chapter about dat.gui library

---
+++ js/interactive-examples/pages/dat-gui/main.js
@@ -0,0 +1,11 @@
+import InteractiveExamplesRunner from "../../InteractiveExamplesRunner";
+
+import './render-scene';
+import './create-panel';
+import './add-input-for-cube-position';
+import './color-input';
+import './checkbox-input';
+import './select-input';
+import './input-with-name';
+
+InteractiveExamplesRunner.init();
