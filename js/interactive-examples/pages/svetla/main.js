2f2e56071ef81460d0532a6af467a224746f220f
Jiří <satorajirka@seznam.cz>
2022-08-11 19:45:30 +0200

    write tutorial chapter about lights

---
+++ js/interactive-examples/pages/svetla/main.js
@@ -0,0 +1,18 @@
+import InteractiveExamplesRunner from "../../InteractiveExamplesRunner";
+
+import './ambient-light';
+import './directional-light';
+import './directional-light-direction';
+import './hemisphere-light';
+import './hemisphere-light-alone';
+import './point-light';
+import './point-light-properties';
+import './spot-light';
+import './spot-light-target';
+import './spot-light-properties';
+import './directional-light-helper';
+import './hemisphere-light-helper';
+import './point-light-helper';
+import './spot-light-helper';
+
+InteractiveExamplesRunner.init();
