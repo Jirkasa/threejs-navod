d97f8f9b71dac5eac26150710dde05f7b47ee9e4
Jiří <satorajirka@seznam.cz>
2022-08-29 09:45:43 +0200

    change formatting of one line in Webpack configuration for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -21,7 +21,8 @@ module.exports = merge(commonConfig, {
                             }
                         }
                     },
-                    "less-loader"]
+                    "less-loader"
+                ]
             }
         ]
     },
